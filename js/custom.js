179de379b9de02c5121f23b84f08fd5169c1d5f1
nuotianyunkong <nuotianyunkong@gmail.com>
2025-12-15 05:00:51 +0800

    Initial commit for GitHub Pages

---
+++ js/custom.js
@@ -0,0 +1,73 @@
+/*-----------------------------------------------------------
+* Template Name    : Busion | Responsive Bootstrap 4 Landing Template
+* Author           : SRBThemes
+* Version          : 2.0.0
+* Created          : February 2018
+* File Description : Main Js file of the template
+*------------------------------------------------------------
+*/
+
+! function($) {
+    "use strict";
+
+    var BusionApp = function() {};
+
+    //scroll
+    BusionApp.prototype.initStickyMenu = function() {
+        $(window).on('scroll',function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 50) {
+                $(".sticky").addClass("stickyadd");
+            } else {
+                $(".sticky").removeClass("stickyadd");
+            }
+        });
+    },
+
+    //Smooth
+    BusionApp.prototype.initSmoothLink = function() {
+        $('.navbar-nav a').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    },
+
+    //ScrollSpy
+    BusionApp.prototype.initScrollspy = function() {
+        $("#navbarCollapse").scrollspy({
+            offset:20
+        });
+    },
+
+
+    //Mfpvideo
+    BusionApp.prototype.inithomepopvideo = function() {
+        $('.watch-video').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    },
+
+    BusionApp.prototype.init = function() {
+        this.initStickyMenu();
+        this.initSmoothLink();
+        this.initScrollspy();
+        this.inithomepopvideo();
+    },
+    //init
+    $.BusionApp = new BusionApp, $.BusionApp.Constructor = BusionApp
+}(window.jQuery),
+
+//initializing
+function($) {
+    "use strict";
+    $.BusionApp.init();
+}(window.jQuery);
